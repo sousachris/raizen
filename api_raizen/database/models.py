0be75e048c8b709fc70d9923b95f2b932e3db839
Christian Carvalho <christian.carvalho@outlook.com>
2024-01-13 20:52:57 -0300

    deleting erros files

--- api_raizen/database/models.py
+++
@@ -1,7 +0,0 @@
-# The examples in this file come from the Flask-SQLAlchemy documentation
-# For more information take a look at:
-# http://flask-sqlalchemy.pocoo.org/2.1/quickstart/#simple-relationships
-
-from datetime import datetime
-
-from rest_api_demo.database import db
